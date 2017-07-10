61e395bee089d05c0bf13338e9ebac481afbc0b7
KARTHIKRAJAJAGADESH <karthikraja1997@rediffmail.com>
2017-07-10 12:34:54 +0530

    Create NAT.java

---
+++ NAT.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class Natural{
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number");
+    int a,i=1,sum=0;
+    a=sc.nextInt();
+   for(i=0;i<=a;i++)
+    {
+      sum = sum+i;
+      //i++;
+      }
+      System.out.println("the natural number is " +sum);
+      }
+      }
