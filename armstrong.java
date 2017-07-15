4bf8a551486280e1119b9a3e732e09dc566d6d1d
KARTHIKRAJAJAGADESH <karthikraja1997@rediffmail.com>
2017-07-15 11:23:54 +0530

    Create armstrong.java

---
+++ armstrong.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+    public class Armstrong{  
+      public static void main(String[] args) throws IOException {  
+        int c=0,a,temp;  
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());                 //It is the number to check armstrong  
+        temp=n;  
+        while(n>0)  
+        {  
+        a=n%10;  
+        n=n/10;  
+        c=c+(a*a*a);  
+        }  
+        if(temp==c)  
+        System.out.println("armstrong number");   
+        else  
+            System.out.println("Not armstrong number");   
+       }  
+    }  
